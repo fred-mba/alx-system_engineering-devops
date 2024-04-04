3322e9b9c98fa4dd63d9dd2d625cfcfd759ebdba
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-04 07:47:29 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,17 @@
 # Client configuration with Puppet
 
-ssh::client::config { 'PasswordAuthentication':
+include stdlib
+
+file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
-  value => 'no',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^#?(\s*IdentityFile\s+~/.ssh/school)',
+  after => '# This is the ssh client system-wide configuration file.  See',
+}
+
+file_line { 'Turn off password authentication':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^#?(\s*PasswordAuthentication\s+yes)',
+  after => '# This is the ssh client system-wide configuration file.  See',
 }
