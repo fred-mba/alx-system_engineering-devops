25d9954e205b2bd4c86e72ad0ff81d84f3ca1748
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-04 07:32:45 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,5 @@
 # Client configuration with Puppet
 
-ssh::client::config { 'IdentityFile':
-  path  => '/etc/ssh/ssh_config',
-  value => '~/.ssh/school',
-}
-
 ssh::client::config { 'PasswordAuthentication':
   path  => '/etc/ssh/ssh_config',
   value => 'no',
