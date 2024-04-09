45d062a31e33f173b3624c547a9f1352bda58377
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-09 16:08:06 +0300

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,19 +6,6 @@ package { 'nginx':
   provider => 'apt', # Package provider
 }
 
-# Enabling firewall on nginx server
-# firewall { '100 allow http':
-  # port  => 80,
-  # proto => 'tcp',
-  # action > 'accept',
-# }
-
-firewall { '100 allow ssh':
-  port   => 22.
-  proto  => 'tcp',
-  action => enable
-}
-
 # After installation, add a query page that contains 'Hello World!'
 file { '/var/www/html/index.html':
   ensure  => present,
@@ -28,8 +15,7 @@ file { '/var/www/html/index.html':
 # Redirection 301 moved permanently
 file { '/etc/nginx/sites-enabled/default':
   ensure  => present,
-  content => "
-  server {
+  content => "server {
     listen 80 default_server;
     listen [::]:80 default_server;
 
