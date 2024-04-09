3c96dea0aa509a7b222d73b17806f436b2a953f1
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-09 11:28:57 +0300

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,49 +1,68 @@
-# Using puppet to manage my nginx configurations
+# Puppet file to manage Nginx installation and configurations
 
 # Install Nginx package
 package { 'nginx':
-  ensure => installed,
+  ensure => 'installed',
 }
 
-# Configure the Nginx Server
-file { '/etc/nginx/sites-available/default':
+# Enabling firewall on nginx server
+exec { 'enable_ufw':
+  command => 'sudo ufw enable',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+exec { 'allow_nginx_http':
+  command => 'sudo ufw allow "Nginx HTTP"',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+exec { 'allow_ssh':
+  command => 'sudo ufw allow ssh',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# After installation, add a query page that contains 'Hello World!'
+file { '/var/www/html/index.html':
   ensure  => present,
-  content => "
-    server {
-	    listen 80;
-		listen [::]:80;
-		
-		server_name 54.237.9.122;
-
-		root /var/www/html;
-		index index.html index.htm index.nginx-debian.html;
-
-		location / {
-			return 200 'Hello World!';
-		}
-
-		location /redirect_me {
-			return 301 'https://www.youtube.com/watch?v=QH2-TGUlwu4';
-		}
-	}
-  ",
-    notify => Exec['nginx_restart'],
+  content => 'Hello World!',
 }
 
+# Redirection 301 moved permanently
 file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
+  ensure  => present,
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    location /redirect_me {
+      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /error_404.html;
+        location = /error_404.html {
+            root /usr/share/nginx/html;
+            internal;
+    }
+  }",
 }
 
+# 404 query message
+file { '/usr/share/nginx/html/error_404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
+
+# Restart Nginx service
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+  ensure    => running,
+  enable    => true,
+  require   => [Package['nginx'], File['/etc/nginx/sites-enabled/default']],
+  subscribe => File['/etc/nginx/sites-enabled/default'],
 }
 
-exec { 'nginx_restart':
+# Execute the restart command
+exec { 'service nginx restart':
   command     => 'service nginx restart',
-  path        => '/usr/sbin:/usr/bin:/sbin:/bin',
+  path        => '/usr/bin:/usr/sbin:/bin',
   refreshonly => true,
 }
