7e59cdcba339920e39e77eb6572990b46dde1a09
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-30 16:27:50 +0300

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+An  extend Python script to export data in the CSV format
+"""
+import requests
+import csv
+import sys
+
+
+def export_to_csv(user_id):
+    base_url = "https://jsonplaceholder.typicode.com/users/"
+    user_url = f"{base_url}{user_id}"
+    user_data = requests.get(user_url).json()
+
+    todos_url = f"{user_url}/todos"
+    todos_data = requests.get(todos_url).json()
+
+    filename = f"{user_id}.csv"
+
+    fieldnames = ["id", "username", "completed", "title"]
+    csv_data = []
+
+    for data in todos_data:
+        csv_data.append({
+            "id": data['userId'],
+            "username": user_data['username'],
+            "completed": data['completed'],
+            "title": data['title']
+        })
+
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer.writerows(csv_data)
+
+
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    export_to_csv(user_id)
