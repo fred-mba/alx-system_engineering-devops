db6865a6e7e1cf7765ba439f4fb38d66c9b03832
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-13 14:36:25 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,8 +2,6 @@
 """
 This module sends a get request to Reddit API to retrieve number of subscribers
 """
-
-
 import requests
 
 
@@ -16,7 +14,8 @@ def number_of_subscribers(subreddit):
 
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    response = requests.get(subreddit_url, headers=headers)
+    response = requests.get(
+        subreddit_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
 
